dc40bf00e5d45fa111696892e9ecbb616645ace8
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-02 08:52:24 -0700

    Add ParksApiContext and dbset for models

---
+++ ParksApi/Models/ParksApiContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksApi.Models
+{
+  public class ParksApiContext : DbContext
+  {
+    public ParksApiContext(DbContextOptions<ParksApiContext> options) : base(options) { }
+    
+    public DbSet<Park> Parks { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+  } 
+}
