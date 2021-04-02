5c682ec110f033f4954edb9dbab3f244381c9a95
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-02 10:13:42 -0700

    Add GET route with multiple query parameters

--- ParksApi/Controllers/ReviewsController.cs
+++ ParksApi/Controllers/ReviewsController.cs
@@ -1,9 +1,9 @@
-using System.Data.Common;
-using System;
+// using System.Data.Common;
+// using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParksApi.Models;
@@ -18,6 +18,26 @@ namespace ParksApi.Controllers
     public ReviewsController(ParksApiContext db)
     {
       _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Review>>> GetAction(int rating, string reviewTitle, string reviewBody)
+    {
+      var query = _db.Reviews.AsQueryable();
+      if (rating == 1 || rating == 2 || rating == 3 || rating == 4 || rating == 5)
+      {
+        query = query.Where(entry => entry.Rating == rating);
+      }
+      if (reviewTitle != null)
+      {
+        query = query.Where(entry => entry.ReviewTitle == reviewTitle);
+      }
+      if (reviewBody != null)
+      {
+        query = query.Where(entry => entry.ReviewBody == reviewBody);
+      }
+
+      return await query.ToListAsync();
     }
 
   }
