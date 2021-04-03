224ee7885abddc332917f4ee310f5cbefb695b49
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-02 23:16:56 -0700

    Add validation review properties

--- ParksApi/Models/Review.cs
+++ ParksApi/Models/Review.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksApi.Models
 {
   public class Review
   {
     public int ReviewId {get; set; }
+    [Required]
     public int Rating { get; set; }
+    [Required]
     public string ReviewTitle { get; set; }
+    [Required]
     public string ReviewBody { get; set; }
+    [Required]
     public int ParkId { get; set; }
   }
 }
