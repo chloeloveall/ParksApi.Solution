78c45ea65b7c5b5e7b379f72308a8d36d6042a46
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-02 23:50:37 -0700

    Add additional properties to park model

---
+++ ParksApi/Migrations/20210403064815_add properties to park model.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class addpropertiestoparkmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewTitle",
+                table: "Reviews",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewBody",
+                table: "Reviews",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParkState",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParkCategory",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParkDescription",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParkName",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParkCategory",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "ParkDescription",
+                table: "Parks");
+
+            migrationBuilder.DropColumn(
+                name: "ParkName",
+                table: "Parks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewTitle",
+                table: "Reviews",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewBody",
+                table: "Reviews",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParkState",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
